84d869a76068b023f87e618daa4aaba4a74771b3
Coders-Ghost <118267956+Coders-Ghost@users.noreply.github.com>
2022-11-21 18:20:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const hamburger = document.querySelector(".container");
+const mobilenav = document.querySelector(".mobilenav");
+console.log('clicked');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('.change')
+    mobilenav.classList.toggle('.is-active');
+    console.log("clicked");
+})
+
+
+/*https://www.instagram.com/ishita_mishra_03/
+
+> GHOST:
+https://www.instagram.com/sonone_ansh08/
+*/
